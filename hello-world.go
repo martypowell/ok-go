42219e885bc5e81aca81df0c3efee4dcc11a4ec6
Marty Powell <jpowel25@gmail.com>
2020-06-06 09:58:33 -0400

    added function for logging animals (using range)

--- hello-world.go
+++ hello-world.go
@@ -9,14 +9,26 @@ import (
 func main() {
 	const messagePrefix string = "hello world"
 
+	animals := []string{"Dog", "Cat", "Elephant"}
+
 	timesToRun :=
 		flag.Int("timesToRun", 1, "Number of times you want to display a message.")
 	message := messagePrefix + " marty in variable, with constant"
 
 	flag.Parse()
 
+	// Print message based on the number of times to run
 	for i := 0; i < *timesToRun; i++ {
 		fmt.Println(message + ", ran " + strconv.Itoa(i+1) + " times")
 	}
 
+	logAnimals(animals)
+}
+
+// Write a list of animals to the terminal
+func logAnimals(animals []string) {
+	// Loop through animals
+	for _, animal := range animals {
+		fmt.Println(animal)
+	}
 }
