7031ecf51b02ef0ee3d618cc295d0fad0c14dbf9
Marty Powell <jpowel25@gmail.com>
2020-06-06 08:23:04 -0400

    cahnges times to run to use args

--- hello-world.go
+++ hello-world.go
@@ -1,17 +1,22 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"strconv"
 )
 
 func main() {
-	const numberOfTimesToMessage = 2
+
+	timesToRun :=
+		flag.Int("timesToRun", 1, "Number of times you want to display a message.")
 	const messagePrefix string = "hello world"
 	var message = messagePrefix + " marty in variable, with constant"
 
-	for i := 0; i < numberOfTimesToMessage; i++ {
-		fmt.Println(message + ", ran " + strconv.Itoa(i) + " times")
+	flag.Parse()
+
+	for i := 0; i < *timesToRun; i++ {
+		fmt.Println(message + ", ran " + strconv.Itoa(i+1) + " times")
 	}
 
 }
