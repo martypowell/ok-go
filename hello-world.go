dc1d7bbcc5af74949cdab7f41a186cd5412595cd
Marty Powell <jpowel25@gmail.com>
2020-06-06 08:12:20 -0400

    converted to run the message in a loop

--- hello-world.go
+++ hello-world.go
@@ -1,10 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 func main() {
+	const numberOfTimesToMessage = 2
 	const messagePrefix string = "hello world"
 	var message = messagePrefix + " marty in variable, with constant"
 
-	fmt.Println(message)
+	for i := 0; i < numberOfTimesToMessage; i++ {
+		fmt.Println(message + ", ran " + strconv.Itoa(i) + " times")
+	}
+
 }
