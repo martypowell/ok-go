18e3e6cc418fdcd135443f9e886373fe5cf1d68d
Marty Powell <jpowel25@gmail.com>
2020-06-06 11:24:01 -0400

    added a test file, to test testing

---
+++ math_test.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestGetBasicArithmatic(t *testing.T) {
+	var tests = []struct {
+		a, b, add, sub, multiply int
+		divide                   float64
+	}{
+		{1, 1, 2, 0, 1, 1},
+	}
+
+	for _, test := range tests {
+		testname := fmt.Sprintf("%d,%d", test.a, test.b)
+		t.Run(testname, func(t *testing.T) {
+			ansAdd, ansSub, ansMultiply, _ := getBasicArithmatic(test.a, test.b)
+
+			if ansAdd != test.add {
+				t.Errorf("got %d, want %d", ansAdd, test.add)
+			}
+			if ansSub != test.sub {
+				t.Errorf("got %d, want %d", ansSub, test.sub)
+			}
+			if ansMultiply != test.multiply {
+				t.Errorf("got %d, want %d", ansMultiply, test.multiply)
+			}
+			// if ansDivide != test.divide {
+			// 	t.Errorf("got %d, want %d", ansDivide, test.divide)
+			// }
+		})
+	}
+
+}
+
+// for _, tt := range tests {
+
+//     t.Run(testname, func(t *testing.T) {
+//         ansAdd, ansSub, ansMultiply, ansDivide := getBasicArithmatic(tt.a, tt.b)
+//         if ans != tt.want {
+//             t.Errorf("got %d, want %d", ans, tt.add, tt.sub, tt.multiply, tt.divide)
+//         }
+//     })
+// }
